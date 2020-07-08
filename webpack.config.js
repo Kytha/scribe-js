ed8d1f374e0e9f93f7893bd68b04ad15470cf456
Kytha <thatchek@mcmaster.ca>
2020-07-08 09:54:10 -0400

    Removed example.

--- webpack.config.js
+++ webpack.config.js
@@ -3,59 +3,51 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 var OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
-  entry: ["./src/index.js", "./src/athena/athena.scss"],
+  entry: ["./src/index.js", "./src/index.scss"],
   output: {
-    path: path.resolve(__dirname, "static"),
-    filename: "index.js"
+    path: path.resolve(__dirname, "lib"),
+    filename: "index.js",
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
+        loader: "babel-loader",
       },
       {
         test: /\.(sa|sc|c)ss$/,
         use: [
           {
-            loader: MiniCssExtractPlugin.loader
+            loader: MiniCssExtractPlugin.loader,
           },
           {
-            loader: "css-loader"
+            loader: "css-loader",
           },
           {
-            loader: "postcss-loader",
-            options: {
-              config: {
-                path: "postcss.config.js"
-              }
-            }
+            loader: "sass-loader",
           },
-          {
-            loader: "sass-loader"
-          }
-        ]
-      }
-    ]
+        ],
+      },
+    ],
   },
   resolve: {
     extensions: ["*", ".js", ".jsx"],
     alias: {
-      react: path.resolve("./node_modules/react")
-    }
+      react: path.resolve("./node_modules/react"),
+    },
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "athena.bundle.css"
+      filename: "scribe.css",
     }),
     new OptimizeCssAssetsPlugin({
       assetNameRegExp: /\.css$/g,
       cssProcessor: require("cssnano"),
       cssProcessorPluginOptions: {
-        preset: ["default", { discardComments: { removeAll: true } }]
+        preset: ["default", { discardComments: { removeAll: true } }],
       },
-      canPrint: true
-    })
-  ]
+      canPrint: true,
+    }),
+  ],
 };
